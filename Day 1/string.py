d05db011b4e012b81b54c89f292dfc3f46ba915e
Atharva-Sarnaik <sarnaikatharva13@gmail.com>
2024-08-23 20:20:00 +0530

    Update string.py

--- Day 1/string.py
+++ Day 1/string.py
@@ -4,10 +4,25 @@ print("Hello",name)
 print("He said\"I want to buy a laptop\"")
 # Using single quoted ('') string to use double quotes inside a string
 print('He said "I want to buy a laptop"')
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+#print(name[7]) #Throws an error as no letter available at the index
 
 #Output
 '''
 Hello Atharva
 He said"I want to buy a laptop"
 He said "I want to buy a laptop"
+A
+t
+h
+a
+r
+v
+a
 ''''
