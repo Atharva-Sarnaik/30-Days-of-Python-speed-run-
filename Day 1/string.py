6934f06fa7124ceedbf0e35adfb064e435b2f2e5
Atharva-Sarnaik <sarnaikatharva13@gmail.com>
2024-08-23 20:10:09 +0530

    Create string.py

---
+++ Day 1/string.py
@@ -0,0 +1,13 @@
+name="Atharva"
+print("Hello",name)
+# Using Escape sequence "\" to use double quote ("") in string
+print("He said\"I want to buy a laptop\"")
+# Using single quoted ('') string to use double quotes inside a string
+print('He said "I want to buy a laptop"')
+
+#Output
+'''
+Hello Atharva
+He said"I want to buy a laptop"
+He said "I want to buy a laptop"
+''''
