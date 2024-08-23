83cb3d8818a57745dec9b2aa69d0b0d88726460d
Atharva-Sarnaik <sarnaikatharva13@gmail.com>
2024-08-23 13:14:32 +0530

    Create Opeators.py

---
+++ Day 1/Opeators.py
@@ -0,0 +1,70 @@
+# Types Of Operators
+# Arithmetic Operators
+
+a=25
+b=36
+print("The Addition is:",a+b)
+print("The Substraction is:",a-b)
+print("The Division is:",a/b)
+print("The Multiplication is:",a*b)
+print("The Remainder is:",a%b)
+print("The Exponent is:",a**b)
+print("The Floor Division is:",a//b)
+
+# Output
+'''
+The Addition is: 61
+The Substraction is: -11
+The Division is: 0.6944444444444444
+The Multiplication is: 900
+The Remainder is: 25
+The Exponent is: 211758236813575084767080625169910490512847900390625
+The Floor Division is: 0
+'''
+# Assignment Operator
+
+a=23
+b=36
+a+=b
+print(a)
+a-=b
+print(a)
+a*=b
+print(a)
+a%=b
+print(a)
+a**=b
+print(a)
+a//=b
+print(a)
+
+# Output
+'''
+59
+23
+828
+0
+0
+0
+'''
+
+# Bitwise Operator
+
+a=5
+b=6
+print("a&b:",a&b)
+print("a|b:",a|b)
+print("a^b:",a^b)
+print("~a:",~a)
+print("a<<b:",a<<b)
+print("a>>b:",a>>b
+
+# Output
+'''
+a&b: 4
+a|b: 7
+a^b: 3
+~a: -6
+a<<b: 320
+a>>b: 0
+'''
