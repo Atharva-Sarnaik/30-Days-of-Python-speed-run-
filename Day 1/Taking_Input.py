7d96d4d95edc2f57f08afe9497d05a185e7d3222
Atharva-Sarnaik <sarnaikatharva13@gmail.com>
2024-08-23 19:43:53 +0530

    Create Taking_Input.py

---
+++ Day 1/Taking_Input.py
@@ -0,0 +1,17 @@
+# Taking user input using input() function
+a1=input("Enter your name:")
+print("My name is",a1)
+a=input("Enter 1st value:")
+b=input("Enter 2nd value:")
+print(a+b)
+print(int(a)+int(b))
+
+#Output
+'''
+Enter your name:Atharva
+My name is Atharva
+Enter 1st value:21
+Enter 2nd value:56
+2156
+77
+''''
