1057f8f706685c8a9c29537a74aadcb5660554ef
Atharva-Sarnaik <sarnaikatharva13@gmail.com>
2024-08-25 16:05:19 +0530

    Update timestamp.py

--- Day 2/timestamp.py
+++ Day 2/timestamp.py
@@ -11,10 +11,23 @@ print(timestamp)
 timestamp=time.strftime("%S")
 print(timestamp)
 
+# Using if else to get Output according to the time.
+
+import time
+timestamp=(time.strftime("%H/%M/%S"))
+print(timestamp)
+timestamp=int(time.strftime("%H"))
+if(timestamp<12):
+    print("Good Moring sir its",timestamp,"am now")
+elif(timestamp>12):
+    print("Good Afternoon sir its",timestamp,"pm now")
+
 # Output
 '''
 15/43/08
 15
 43
 08
+15/55/27
+Good Afternoon sir its 15 pm now
 '''
