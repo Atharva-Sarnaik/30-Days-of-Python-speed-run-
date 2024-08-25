5c94cc902b5282ec0d0a8122eb65a7e729e603b6
Atharva-Sarnaik <sarnaikatharva13@gmail.com>
2024-08-25 14:30:50 +0530

    Create if-else.py

---
+++ Day 2/if-else.py
@@ -0,0 +1,20 @@
+# if-else statement
+
+age=int(input("Enter Your Age:"))
+print("Your age is",age)
+if age>=25:
+    print("You can drink!")
+    print('YES!') 
+else:
+    print("You cannot drink!")
+    print("NO!")#Space after the else block means "indentation" in python which means that part is included in else block
+print("YAY!") # This will be printed regardless of if-else
+
+# Output
+'''
+Enter Your Age:18
+Your age is 18
+You can drive!
+YES!
+YAY!
+'''
