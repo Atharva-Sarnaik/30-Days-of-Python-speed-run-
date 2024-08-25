13c1bc1f4ae196a0503f3abcf3e2dfd5320763c0
Atharva-Sarnaik <sarnaikatharva13@gmail.com>
2024-08-25 14:42:25 +0530

    Create elif.py

---
+++ Day 2/elif.py
@@ -0,0 +1,20 @@
+# Elif statement
+
+num=int(input("Enter the value of num:"))
+print("Entered value is:",num)
+if(num>0):
+    print('Number is positive')
+elif(num==7722):
+    print('Number is special')
+else:
+    print('Number is negative')
+
+print("I'm Happy Now")
+
+# Output
+'''
+Enter the value of num:24
+Entered value is: 24
+Number is positive
+I'm Happy Now
+'''
