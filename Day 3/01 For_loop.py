b918b9cef3a5ee493fdb4f76d4a700360c259ed7
Atharva-Sarnaik <sarnaikatharva13@gmail.com>
2024-08-27 22:36:11 +0530

    Create 01 For_loop.py

---
+++ Day 3/01 For_loop.py
@@ -0,0 +1,72 @@
+# For loop in string
+car="Lamborghini"
+for i in car:
+    print(i)
+
+# For loop in list
+a=["john","matt","lily","becky"]
+for j in a:
+    print(j)
+    for k in j:
+        print(k)
+
+# Range function in for loop
+for i in range(5):
+    print(i)
+for i in range(5):
+    print(i+1)
+
+for j in range(1,12,2):
+    print(j)
+
+# Output
+'''
+L
+a
+m
+b
+o
+r
+g
+h
+i
+n
+i
+john
+j
+o
+h
+n
+matt
+m
+a
+t
+t
+lily
+l
+i
+l
+y
+becky
+b
+e
+c
+k
+y
+0
+1
+2
+3
+4
+1
+2
+3
+4
+5
+1
+3
+5
+7
+9
+11
+'''
